8ca068a70351280dfd306da8db12ea28b1d0ee48
Aeneas Rekkas (arekkas) <aeneas@ory.am>
2017-04-27 12:12:11 +0200

    refactor herodot and write docs

---
+++ trace.go
@@ -0,0 +1,23 @@
+package herodot
+
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/pkg/errors"
+)
+
+type stackTracer interface {
+	StackTrace() errors.StackTrace
+}
+
+// getErrorTrace is a helper that returns an error's trace
+func getErrorTrace(err error) string {
+	if e, ok := err.(stackTracer); ok {
+		return fmt.Sprintf("Stack trace: %+v", e.StackTrace())
+	} else if e, ok := errors.Cause(err).(stackTracer); ok {
+		return fmt.Sprintf("Stack trace: %+v", e.StackTrace())
+	}
+
+	return fmt.Sprintf("Stack trace could not be recovered from error type %s", reflect.TypeOf(err))
+}
