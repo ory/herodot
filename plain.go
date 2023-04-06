fc7f23f675f5bd1435c4a1859b044ffac72bd4c9
Arne Luenser <arne.luenser@ory.sh>
2023-04-06 16:26:23 +0200

    fix: add type assert to verify JSONWriter and TextWriter actually implement the Writer interface (which TextWriter in fact didn't)

--- plain.go
+++ plain.go
@@ -10,13 +10,15 @@ import (
 	"github.com/pkg/errors"
 )
 
-// json outputs JSON.
+// TextWriter outputs plain text
 type TextWriter struct {
 	Reporter    ErrorReporter
 	contentType string
 }
 
-// NewPlainWriter returns a json
+var _ Writer = (*TextWriter)(nil)
+
+// NewTextWriter returns a plain text writer
 func NewTextWriter(reporter ErrorReporter, contentType string) *TextWriter {
 	if contentType == "" {
 		contentType = "plain"
@@ -31,12 +33,12 @@ func NewTextWriter(reporter ErrorReporter, contentType string) *TextWriter {
 }
 
 // Write a response object to the ResponseWriter with status code 200.
-func (h *TextWriter) Write(w http.ResponseWriter, r *http.Request, e interface{}) {
+func (h *TextWriter) Write(w http.ResponseWriter, r *http.Request, e interface{}, _ ...EncoderOptions) {
 	h.WriteCode(w, r, http.StatusOK, e)
 }
 
 // WriteCode writes a response object to the ResponseWriter and sets a response code.
-func (h *TextWriter) WriteCode(w http.ResponseWriter, r *http.Request, code int, e interface{}) {
+func (h *TextWriter) WriteCode(w http.ResponseWriter, r *http.Request, code int, e interface{}, _ ...EncoderOptions) {
 	if code == 0 {
 		code = http.StatusOK
 	}
@@ -56,29 +58,25 @@ func (h *TextWriter) WriteCreated(w http.ResponseWriter, r *http.Request, locati
 // WriteError writes an error to ResponseWriter and tries to extract the error's status code by
 // asserting statusCodeCarrier. If the error does not implement statusCodeCarrier, the status code
 // is set to 500.
-func (h *TextWriter) WriteError(w http.ResponseWriter, r *http.Request, err interface{}) {
-	if s, ok := errors.Cause(toError(err)).(statusCodeCarrier); ok {
+func (h *TextWriter) WriteError(w http.ResponseWriter, r *http.Request, err error, _ ...Option) {
+	if s, ok := errors.Cause(coalesceError(err)).(StatusCodeCarrier); ok {
 		h.WriteErrorCode(w, r, s.StatusCode(), err)
 		return
+	} else {
+		h.WriteErrorCode(w, r, http.StatusInternalServerError, err)
 	}
-
-	h.WriteErrorCode(w, r, http.StatusInternalServerError, err)
-	return
 }
 
 // WriteErrorCode writes an error to ResponseWriter and forces an error code.
-func (h *TextWriter) WriteErrorCode(w http.ResponseWriter, r *http.Request, code int, err interface{}) {
-	e := toError(err)
-	if err == nil {
-		err = e
-	}
+func (h *TextWriter) WriteErrorCode(w http.ResponseWriter, r *http.Request, code int, err error, _ ...Option) {
+	err = coalesceError(err)
 
 	if code == 0 {
 		code = http.StatusInternalServerError
 	}
 
 	// All errors land here, so it's a really good idea to do the logging here as well!
-	h.Reporter.ReportError(r, code, e, "An error occurred while handling a request")
+	h.Reporter.ReportError(r, code, err, "An error occurred while handling a request")
 
 	w.Header().Set("Content-Type", h.contentType)
 	w.WriteHeader(code)
