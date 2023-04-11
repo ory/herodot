b261953f9415f4a9bec2076a503aca2beff00361
Arne Luenser <arne.luenser@ory.sh>
2023-04-11 15:44:22 +0200

    fix: remove MarshalJSON implementation from DefaultError
    This interferes with embedding *DefaultError in custom error structs.

--- error_default.go
+++ error_default.go
@@ -4,7 +4,6 @@
 package herodot
 
 import (
-	"encoding/json"
 	stderr "errors"
 	"fmt"
 	"io"
@@ -68,16 +67,6 @@ type DefaultError struct {
 
 	GRPCCodeField codes.Code `json:"-"`
 	err           error
-	enableDebug   bool
-}
-
-func (e *DefaultError) MarshalJSON() ([]byte, error) {
-	type alias DefaultError
-	ee := alias(*e)
-	if !ee.enableDebug {
-		ee.DebugField = ""
-	}
-	return json.Marshal(&ee)
 }
 
 // StackTrace returns the error's stack trace.
